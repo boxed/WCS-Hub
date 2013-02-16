e5ece92bead2d9039b7d8b0b36f35f8a061fa91e
Anders Hovmöller <boxed@killingar.net>
2013-02-16 15:38:10 +0100

    use chief judge scores in the very rare case of a multi-tie, added tests for that, refactoring of code to make it easier to follow

--- relative_placement.py
+++ relative_placement.py
@@ -4,12 +4,17 @@ from math import ceil
 
 ### C. Tallying the Final Placements
 
+class CoupleScoring(object):
+    def __init__(self, name, judges_scores, chief_judge_score, user_data):
+        self.name, self.judges_scores, self.chief_judge_score, self.user_data = name, judges_scores, chief_judge_score, user_data
+
 ### 1. In the finals, each Judge must place every couple in rank order (1st place, 2nd place, 3rd place, etc.).
 def validate_scores(data):
     judges_data = {}
     for row in data:
-        row[1] = [cell or len(data)+1 for cell in row[1]]
-        for i, item in enumerate(row[1]):
+        assert isinstance(row, CoupleScoring)
+        row.judges_scores = [cell or len(data)+1 for cell in row.judges_scores]
+        for i, item in enumerate(row.judges_scores):
             if item:
                 judge_data = judges_data.get(i, [])
                 judge_data.append(item)
@@ -17,28 +22,33 @@ def validate_scores(data):
         ### ...  In finals, with a large field of couples, the Judges will concentrate on placing the top twelve couples.
         ### 3. Duplicate placements are not permitted. If a Judge mistakenly provides duplicate placements, the Scorer will alert the Chief Judge, who will request that the Judge in question provide unique placements for each couple.
         assert sorted(judge_data)[:12] == range(1, len(judge_data[:12])+1)
+    # TODO: validate chief judges data
 
 ### 2. Raw scores (9.5, 8.9, 7.6, etc.) are used only to determine a Judge's order of placements. If a Judge submits only raw scores, the Scorer will convert them into ordinals, (1, 2, 3, etc.) for Relative Placement.
-
 # TODO, probably in validate_scores, since there we know if we've gotten invalid scores
 
 def number_of_votes_and_sum(row, from_placement):
+    assert isinstance(row, CoupleScoring)
     ### 4. A couple must have a majority of Judges' votes to be awarded a final placement.
-    majority_count = int(ceil(len(row[1])/2.0))
+    majority_count = int(ceil(len(row.judges_scores)/2.0))
 
-    votes = sum([1 for x in row[1] if x <= from_placement])
+    votes = sum([1 for x in row.judges_scores if x <= from_placement])
     if votes < majority_count:
         #### 5. If no couple has a majority of votes, then the next placement  is added to the previous placements (1st through 2nd, 1st through 3rd, etc.) until a majority is reached.
         return 0, 0
     ### 6. If two or more couples have an equal majority, then the numerical value of the ordinals for each couple is added. The couple with the lowest sum gets the higher position. 
-    return votes, -sum([x for x in row[1] if x <= from_placement])
+    return votes, -sum([x for x in row.judges_scores if x <= from_placement])
     
 def voting_tabulation(row, places):
     ### 6 continued:... If the sums for two or more couples are identical, then the next placement is added to the previous placements for those tied couples only.
-    return [number_of_votes_and_sum(row, x) for x in xrange(1, places)]
+    return [number_of_votes_and_sum(row, x)+(1.0/row.chief_judge_score if row.chief_judge_score is not None else None,) for x in xrange(1, places)]
 
 # couple ID, judge placements..., final placement
 def calculate_scores(data):
+    if not data:
+        return
+    assert isinstance(data[0], CoupleScoring)
+
     places = len(data)
 
     validate_scores(data)
@@ -47,5 +57,5 @@ def calculate_scores(data):
     return final_result
 
 def print_final_tabulation(final_result):
-    for tabulation, (couple, judge_scores, final_placement) in final_result:
-        print ''.join(['{:>3} ({:>3})'.format(*x) for x in tabulation]), couple, final_placement
+    for tabulation, couple_scoring in final_result:
+        print ''.join(['{:>3} ({:>3})'.format(*x) for x in tabulation]), couple_scoring.chief_judge_score, repr(couple_scoring.name), couple_scoring.user_data
--- tests.py
+++ tests.py
@@ -5,77 +5,88 @@ class Test(TestCase):
     def check_data(self, data):
         # print_final_tabulation(calculate_scores(data))
         scores = calculate_scores(data)[:12] # We only care that the first 12 are correct, functionally the rest are split last place
-        self.assertEquals([x[-1][-1] for x in scores], range(1, len(scores)+1))
+        self.assertEquals([x[-1].user_data for x in scores], range(1, len(scores)+1))
 
     def test_CDS_2011_int_jj_finals(self):
         data = [
             # couple ID, judge placements..., final placement
-            [('Matthew Taylor', 'Hayley Minkin'),       ( 2,  3,  1,  1,  3,  4,  9),  1],
-            [('Cameron Martinez', 'Erica Piper'),       ( 1,  1,  9,  5,  1,  3,  5),  2],
-            [('Larkin Beanland', 'Jessica Murray'),     ( 3,  2,  6,  2,  7,  7,  1),  3],
-            [('David Guido', 'Leigh Gravette'),         ( 6,  4,  8,  4,  2,  6,  4),  4],
-            [('Forrest Hanson', 'Sammi Sekhorn'),       ( 5, 10,  4,  3,  8,  1,  7),  5],
-            [('David Hemmerich', 'Cassandra Winter'),   ( 4,  9,  5,  6,  4,  2,  8),  6],
-            [('Anders Hovmoller', 'Shantala Davis'),    (10,  5,  7,  7,  9,  5,  3),  7],
-            [('Jonathan Uy', 'Jessica Greer'),          ( 7,  6,  3, 10, 10, 10,  6),  8],
-            [('Marcus Sterling', 'Stephanie Springer'), ( 8,  7, 10,  8,  6,  9,  2),  9],
-            [('William Sawyer', 'Holly Skinner'),       ( 9,  8,  2,  9,  5,  8, 10), 10],
+            CoupleScoring('Matthew Taylor, Hayley Minkin',       ( 2,  3,  1,  1,  3,  4,  9), None,  1),
+            CoupleScoring('Cameron Martinez, Erica Piper',       ( 1,  1,  9,  5,  1,  3,  5), None,  2),
+            CoupleScoring('Larkin Beanland, Jessica Murray',     ( 3,  2,  6,  2,  7,  7,  1), None,  3),
+            CoupleScoring('David Guido, Leigh Gravette',         ( 6,  4,  8,  4,  2,  6,  4), None,  4),
+            CoupleScoring('Forrest Hanson, Sammi Sekhorn',       ( 5, 10,  4,  3,  8,  1,  7), None,  5),
+            CoupleScoring('David Hemmerich, Cassandra Winter',   ( 4,  9,  5,  6,  4,  2,  8), None,  6),
+            CoupleScoring('Anders Hovmoller, Shantala Davis',    (10,  5,  7,  7,  9,  5,  3), None,  7),
+            CoupleScoring('Jonathan Uy, Jessica Greer',          ( 7,  6,  3, 10, 10, 10,  6), None,  8),
+            CoupleScoring('Marcus Sterling, Stephanie Springer', ( 8,  7, 10,  8,  6,  9,  2), None,  9),
+            CoupleScoring('William Sawyer, Holly Skinner',       ( 9,  8,  2,  9,  5,  8, 10), None, 10),
         ]
         self.check_data(data)
 
     def test_CDS_2012_all_stars_jj_finals(self):
         data = [
-            [('Joshua Sturgeon', 'Chevy Slater'),	    (2,	1,	1,	2,	3),	1],
-            [('Brad Whelan', 'Kara Frenzel'),	        (4,	3,	6,	1,	1),	2],
-            [('Christopher Dumond', 'Janelle Guido'),	(3,	2,	2,	6,	5),	3],
-            [('Cameron Crook', 'Jessica Pacheco'),	    (5,	4,	3,	3,	2),	4],
-            [('Bob Tucker', 'Katrina Ostrenski'),	    (7,	6,	5,	5,	4),	5],
-            [('Diego Borges', 'Mackenzie Goodmanson'),	(6,	5,	7,	4,	6),	6],
-            [('Rome Slater', 'Tashina Beckmann'),	    (1,	7,	4,	7,	7),	7],
+            CoupleScoring('Joshua Sturgeon, Chevy Slater',	    (2, 1, 1, 2, 3), None, 1),
+            CoupleScoring('Brad Whelan, Kara Frenzel',	        (4, 3, 6, 1, 1), None, 2),
+            CoupleScoring('Christopher Dumond, Janelle Guido',	(3, 2, 2, 6, 5), None, 3),
+            CoupleScoring('Cameron Crook, Jessica Pacheco',	    (5, 4, 3, 3, 2), None, 4),
+            CoupleScoring('Bob Tucker, Katrina Ostrenski',	    (7, 6, 5, 5, 4), None, 5),
+            CoupleScoring('Diego Borges, Mackenzie Goodmanson', (6, 5, 7, 4, 6), None, 6),
+            CoupleScoring('Rome Slater, Tashina Beckmann',	    (1, 7, 4, 7, 7), None, 7),
         ]
         self.check_data(data)
 
     def test_jose_de_la_mancha(self):
         data = [
-            [('Couple A'),	    (4,	1,	1,	2,	1),	1],
-            [('Couple B'),	    (2,	3,	5,	4,	4),	2],
-            [('Couple C'),	    (1,	5,	4,	1,	5),	3],
-            [('Couple D'),	    (5,	6,	3,	3,	8),	4],
-            [('Couple E'),	    (7,	2,	2,	6,	7),	5],
-            [('Couple F'),	    (6,	8,	8,	5,	2),	6],
-            [('Couple G'),	    (8,	4,	6,	8,	6),	7],
-            [('Couple H'),	    (3,	7,	7,	7,	3),	8],
+            CoupleScoring('Couple A', (4, 1, 1, 2, 1), None, 1),
+            CoupleScoring('Couple B', (2, 3, 5, 4, 4), None, 2),
+            CoupleScoring('Couple C', (1, 5, 4, 1, 5), None, 3),
+            CoupleScoring('Couple D', (5, 6, 3, 3, 8), None, 4),
+            CoupleScoring('Couple E', (7, 2, 2, 6, 7), None, 5),
+            CoupleScoring('Couple F', (6, 8, 8, 5, 2), None, 6),
+            CoupleScoring('Couple G', (8, 4, 6, 8, 6), None, 7),
+            CoupleScoring('Couple H', (3, 7, 7, 7, 3), None, 8),
         ]
         self.check_data(data)
 
     def test_example_from_WSDC_docs(self):
         data = [
-            [('couple 1'), (1, 1, 3, 2, 3), 1],
-            [('couple 2'), (6, 5, 4, 1, 2), 4],
-            [('couple 3'), (2, 4, 1, 5, 5), 3],
-            [('couple 4'), (4, 2, 5, 6, 6), 6],
-            [('couple 5'), (5, 6, 2, 3, 4), 5],
-            [('couple 6'), (3, 3, 6, 4, 1), 2],
+            CoupleScoring('couple 1', (1, 1, 3, 2, 3), None, 1),
+            CoupleScoring('couple 2', (6, 5, 4, 1, 2), None, 4),
+            CoupleScoring('couple 3', (2, 4, 1, 5, 5), None, 3),
+            CoupleScoring('couple 4', (4, 2, 5, 6, 6), None, 6),
+            CoupleScoring('couple 5', (5, 6, 2, 3, 4), None, 5),
+            CoupleScoring('couple 6', (3, 3, 6, 4, 1), None, 2),
+        ]
+        self.check_data(data)
+
+    def test_chief_judge_tie_break(self):
+        data = [
+            CoupleScoring('couple 1', (1, 5, 4, 3, 2), 1, 1),
+            CoupleScoring('couple 2', (2, 1, 5, 4, 3), 2, 2),
+            CoupleScoring('couple 3', (3, 2, 1, 5, 4), 3, 3),
+            CoupleScoring('couple 4', (4, 3, 2, 1, 5), 4, 4),
+            CoupleScoring('couple 5', (5, 4, 3, 2, 1), 5, 5),
+            CoupleScoring('couple 6', (6, 6, 6, 6, 6), 6, 6),
         ]
         self.check_data(data)
 
     def test_only_rank_top_12(self):
         data = [
-            [('couple 1'),  (   1,    1,    1,    1,    1),  1],
-            [('couple 2'),  (   2,    2,    2,    2,    2),  2],
-            [('couple 3'),  (   3,    3,    3,    3,    3),  3],
-            [('couple 4'),  (   4,    4,    4,    4,    4),  4],
-            [('couple 5'),  (   5, None,    5,    5,    5),  5],
-            [('couple 6'),  (   6,    6,    6,    6,    6),  6],
-            [('couple 7'),  (   7,    7,    7,    7,    7),  7],
-            [('couple 8'),  (   8,    8,    8,    8,    8),  8],
-            [('couple 9'),  (   9,    9,    9,    9,    9),  9],
-            [('couple 10'), (  10, None,   10,   10, None), 10],
-            [('couple 11'), (  11,    5, None,   11,   11), 11],
-            [('couple 12'), (  12, None, None,   12,   12), 12],
-            [('couple 13'), (  13, None,   13, None, None), 13],
-            [('couple 14'), (None,   10,   11, None, None), 14],
-            [('couple 15'), (None, None,   12, None,   10), 15],
+            CoupleScoring('couple 1',  (   1,    1,    1,    1,    1), None,  1),
+            CoupleScoring('couple 2',  (   2,    2,    2,    2,    2), None,  2),
+            CoupleScoring('couple 3',  (   3,    3,    3,    3,    3), None,  3),
+            CoupleScoring('couple 4',  (   4,    4,    4,    4,    4), None,  4),
+            CoupleScoring('couple 5',  (   5, None,    5,    5,    5), None,  5),
+            CoupleScoring('couple 6',  (   6,    6,    6,    6,    6), None,  6),
+            CoupleScoring('couple 7',  (   7,    7,    7,    7,    7), None,  7),
+            CoupleScoring('couple 8',  (   8,    8,    8,    8,    8), None,  8),
+            CoupleScoring('couple 9',  (   9,    9,    9,    9,    9), None,  9),
+            CoupleScoring('couple 10', (  10, None,   10,   10, None), None, 10),
+            CoupleScoring('couple 11', (  11,    5, None,   11,   11), None, 11),
+            CoupleScoring('couple 12', (  12, None, None,   12,   12), None, 12),
+            CoupleScoring('couple 13', (  13, None,   13, None, None), None, 13),
+            CoupleScoring('couple 14', (None,   10,   11, None, None), None, 14),
+            CoupleScoring('couple 15', (None, None,   12, None,   10), None, 15),
         ]
         self.check_data(data)
 
